056383c5736299515cb52517344017b6d5ef08ad
tensence127 <104569220+tensence127@users.noreply.github.com>
2024-10-22 03:26:42 +0700

    Create Circle.cpp

---
+++ Circle.cpp
@@ -0,0 +1,27 @@
+#include "Circle.h"
+
+Circle::Circle(double r)
+{
+    radius = new double;
+    *radius = r;
+}
+
+double Circle::calcArea()
+{
+    return acos(-1)*pow(*radius, 2);
+}
+
+void Circle::show()
+{
+    cout << "Circle with radius: " << *radius;
+}
+
+Figure* Circle::Copy()
+{
+    return new Circle(*radius);
+}
+
+Circle::~Circle()
+{
+    delete radius;
+}
